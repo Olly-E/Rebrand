0fb5206bf4edfa239aa64fabf8d59d0921aa9851
qgatssdev <chivoomodu@gmail.com>
2024-01-18 16:12:11 +0100

    feat: templates animations

--- src/app/Components/Templates.tsx
+++ src/app/Components/Templates.tsx
@@ -10,30 +10,64 @@ export const Templates = () => {
 
   useEffect(() => {
     const ctx = gsap.context(() => {
-      const tl = gsap.timeline({})
-
-      const sections = gsap.utils.toArray('.horizontal-scroll-container')
-
-      tl.to(sections, {
-        xPercent: -57 * (sections.length - 1),
-        ease: 'none',
+      const masterTimeline = gsap.timeline({
         scrollTrigger: {
           trigger: containerRef.current,
           pin: true,
           start: 'top top',
           scrub: 1,
-          end: '+=3000',
+          end: '+=5000',
         },
       })
-
-      return () => tl.kill()
+      const sections = gsap.utils.toArray(
+        '.picture-container'
+      ) as HTMLDivElement[]
+      sections.forEach((section, index) => {
+        const sectionTimeline = gsap.timeline()
+        sectionTimeline.fromTo(
+          section,
+          {
+            y: '100vh',
+            scale: 1.5,
+          },
+          { y: -index * 100, ease: 'none' },
+          0
+        )
+        sectionTimeline.to(section, {
+          scale: 1,
+        })
+        masterTimeline.add(sectionTimeline, index * 0.7)
+      })
+      return () => masterTimeline.kill()
     }, containerRef)
-
     return () => ctx.revert()
   }, [])
 
+  const TEMPLATE_CONTENT_DATA = [
+    {
+      name: 'CARTIZN',
+      category: 'BRANDING/WEB DESIGN/DEVELOPMENT',
+      img: '/assets/home/project1.png',
+    },
+    {
+      name: 'DETAIL DIVE',
+      category: 'BRANDING/WEB DESIGN',
+      img: '/assets/home/project2.png',
+    },
+    {
+      name: 'FUTURE FORGE LABS',
+      category: 'BRAND LOGO CONCEPT',
+      img: '/assets/home/project3.png',
+    },
+    {
+      name: 'THELLY’S DELIGHT',
+      category: 'BRANDING',
+      img: '/assets/home/project4.png',
+    },
+  ]
+
   return (
-    <section className="h-screen" ref={containerRef}>
+    <section className="h-screen mb-[100px]" ref={containerRef}>
       <h3 className="text-[230px] text-center text-white-state leading-[180px] pt-[10px]">
         TEMPLATES
       </h3>
@@ -44,12 +78,34 @@ export const Templates = () => {
         </p>
         <Button className="mt-8 w-[293px]">VIEW TEMPLATES</Button>
       </div>
+      <div className="flex justify-center relative">
+        <div className="absolute">
+          {TEMPLATE_CONTENT_DATA.map((template, index) => {
+            return (
+              <div
+                key={index}
+                className={`w-[617px] picture-container -top-[110px] absolute left-[50%] -translate-x-[50%] z-[${
+                  index * 1
+                }]`}
+              >
+                <img src={template.img} alt="project-img" width={'100%'} />
+                <div className="flex justify-between">
+                  <p className="text-sm w-[160px]">{template.name}</p>
+                  <p className="text-sm w-[160px] text-right">
+                    {template.category}
+                  </p>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
       <div className="mt-[250px] mr-[5%] flex flex-col items-end">
         <p className="w-[293px]">
-          We create and sell ready-made portfolio designs for designers,
-          developers and other tech enthusiasts
+          Consult our team to design and / or develop a custom-made portfolio
+          for yourself or your brand.
         </p>
-        <Button className="w-[293px]">VIEW TEMPLATES</Button>
+        <Button className="w-[293px]">LET&apos;S CONNECT</Button>
       </div>
     </section>
   )
