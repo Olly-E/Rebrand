e26581b40b6931c8b2d40e53ff6ede797f497d04
qgatssdev <chivoomodu@gmail.com>
2024-01-31 12:37:44 +0100

    feat: generate type for package and add music

---
+++ generated-types/index.d.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { HookOptions, ReturnedValue } from './types'
+export default function useSound<T = any>(
+  src: string | string[],
+  {
+    id,
+    volume,
+    playbackRate,
+    soundEnabled,
+    interrupt,
+    onload,
+    ...delegated
+  }?: HookOptions<T>
+): ReturnedValue
+export { useSound }
